4f51920b31a23eae6f0213640dc104fbd6497482
pandyakaa <pandyaka.aptanagi@gmail.com>
2019-04-19 11:08:51 +0700

    linking front and back

---
+++ FrontEnd/Template/temp.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Flat ChatBox</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <div class="name">
+        <p>
+            ChaChaChatBox
+        </p>    
+    </div>
+
+    <form action="<?php $_PHP_SELF ?>" method="POST">
+
+        <select class="select" name="algorithm" required>
+            <option value="" hidden>Select Algorithm</option>
+            <option value="BM">Boyer-Moore</option>
+            <option value="KMP">KMP</option>
+            <option value="Regex">Regex</option>
+        </select>
+
+        <div id = "container">
+
+            <div class="gif">
+                <img src="bot.gif" width = 200 height = 240 />
+            </div>
+
+            <div class="chatbox">
+
+                <div class="chatlogs">
+
+                        <?php
+                            session_start();
+                            
+                            if (!isset($_SESSION['qna']))
+                                {
+                                    $_SESSION['qna'] = array();
+                                }
+
+                            if (!isset($_SESSION['user']))
+                                {
+                                    $_SESSION['user'] = array();
+                                }
+                            
+                            $url = 'http://127.0.0.1:5000/';
+
+                            if (isset($_POST['inputbox']) )
+                                {
+                                    $res = findAnswer($url,$_POST);
+                                    array_push($_SESSION['qna'],$res); 
+                                    array_push($_SESSION['user'],$_POST['inputbox']);
+                                }
+
+                            function findAnswer($url,$data) 
+                                {
+                                    $ch = curl_init($url);
+
+                                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+                                    $getres = curl_exec($ch); // Masih dalam bentuk json
+
+                                    curl_close($ch);
+                                    
+                                    $finalx = json_decode($getres);
+
+                                    return $finalx;
+                                }
+
+                            if (!empty($_SESSION['user']) && !empty($_SESSION['qna']))
+                                {
+                                    for ($x = 0 ; $x < count($_SESSION['user']) ; $x++)
+                                        { ?>
+                                            <div class="chat self">
+                                                <div class="user-pic">
+                                                    <!img src="2.jpg">
+                                                </div>
+                                                <p class="chat-message">
+                                                    <?php echo($_SESSION['user'][$x]); ?>
+                                                </p>
+                                            </div> 
+
+                                            <div class="chat bot">
+                                                <div class="user-pic">
+                                                    <!img src="1.jpg">
+                                                </div>
+                                                <p class="chat-message">
+                                                    <?php echo($_SESSION['qna'][$x]->data); ?>
+                                                </p>
+                                            </div>
+                                    <?php  }
+                                }
+                        ?>
+
+                </div>
+
+                <div class="chat-form">
+                    <textarea name="inputbox"></textarea>
+                    <button>Send</button>
+                </div>
+
+            </div>
+        </div>
+
+    </form>
+    
+</body>
+</html>
