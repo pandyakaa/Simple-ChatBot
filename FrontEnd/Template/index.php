edae472163feb47648d0cfdb085a8994f04479a7
pandyakaa <pandyaka.aptanagi@gmail.com>
2019-04-21 21:17:57 +0700

    little bit fixing

--- FrontEnd/Template/index.php
+++ FrontEnd/Template/index.php
@@ -1,3 +1,54 @@
+<?php 
+    session_start();
+                            
+    if (!isset($_SESSION['qna']))
+        {
+            $_SESSION['qna'] = array();
+            array_push($_SESSION['qna'], "Hello, may I help you ? :)");
+        }
+
+    if (!isset($_SESSION['user']))
+        {
+            $_SESSION['user'] = array();
+        }
+    
+    $url = 'http://127.0.0.1:5000/';
+
+    if (isset($_POST['inputbox']) )
+        {
+            if ($_POST['inputbox'] === "reset")
+                {
+                    $_SESSION['qna'] = array();
+                    array_push($_SESSION['qna'], "Hello, may I help you ? :)");
+                    $_SESSION['user'] = array();
+                }
+            else
+                {
+                    $res = findAnswer($url,$_POST);
+                    array_push($_SESSION['qna'],$res); 
+                    array_push($_SESSION['user'],$_POST['inputbox']);
+                }
+        }
+
+    function findAnswer($url,$data) 
+        {
+            $ch = curl_init($url);
+
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+            $getres = curl_exec($ch);
+
+            curl_close($ch);
+            
+            $finalx = json_decode($getres);
+
+            return $finalx;
+        }
+
+        unset($_POST);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +64,7 @@
         </p>    
     </div>
 
-    <form action="<?php $_PHP_SELF ?>" method="POST">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 
         <select class="select" name="algorithm" required>
             <option value="" hidden>Select Algorithm</option>
@@ -32,70 +83,41 @@
 
                 <div class="chatlogs">
 
-                        <?php
-                            session_start();
-                            
-                            if (!isset($_SESSION['qna']))
-                                {
-                                    $_SESSION['qna'] = array();
-                                }
+                            <div class="chat bot">
+                                    <div class="user-pic">
+                                        <!img src="1.jpg">
+                                    </div>
+                                    <p class="chat-message">
+                                        <?php echo($_SESSION['qna'][0]); ?>
+                                    </p>
+                            </div>
 
-                            if (!isset($_SESSION['user']))
-                                {
-                                    $_SESSION['user'] = array();
-                                }
-                            
-                            $url = 'http://127.0.0.1:5000/';
-
-                            if (isset($_POST['inputbox']) )
-                                {
-                                    $res = findAnswer($url,$_POST);
-                                    array_push($_SESSION['qna'],$res); 
-                                    array_push($_SESSION['user'],$_POST['inputbox']);
-                                }
-
-                            function findAnswer($url,$data) 
-                                {
-                                    $ch = curl_init($url);
-
-                                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-                                    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-                                    $getres = curl_exec($ch); // Masih dalam bentuk json
-
-                                    curl_close($ch);
-                                    
-                                    $finalx = json_decode($getres);
-
-                                    return $finalx;
-                                }
-
-                            if (!empty($_SESSION['user']) && !empty($_SESSION['qna']))
-                                {
-                                    for ($x = 0 ; $x < count($_SESSION['user']) ; $x++)
-                                        { ?>
-                                            <div class="chat self">
-                                                <div class="user-pic">
-                                                    <!img src="2.jpg">
+                            <?php
+                                if (!empty($_SESSION['user']) && !empty($_SESSION['qna']))
+                                    {
+                                        for ($x = 0 ; $x < count($_SESSION['user']) ; $x++)
+                                            { ?>
+                                                <div class="chat self">
+                                                    <div class="user-pic">
+                                                        <!img src="2.jpg">
+                                                    </div>
+                                                    <p class="chat-message">
+                                                        <?php echo($_SESSION['user'][$x]); ?>
+                                                    </p>
                                                 </div>
-                                                <p class="chat-message">
-                                                    <?php echo($_SESSION['user'][$x]); ?>
-                                                </p>
-                                            </div> 
 
-                                            <div class="chat bot">
-                                                <div class="user-pic">
-                                                    <!img src="1.jpg">
+                                                <div class="chat bot">
+                                                    <div class="user-pic">
+                                                        <!img src="1.jpg">
+                                                    </div>
+                                                    <p class="chat-message">
+                                                        <?php echo($_SESSION['qna'][$x+1]->data); ?>
+                                                    </p>
                                                 </div>
-                                                <p class="chat-message">
-                                                    <?php echo($_SESSION['qna'][$x]->data); ?>
-                                                </p>
-                                            </div>
-                                    <?php  }
-                                }
-                            
-                        ?>
 
+                                        <?php  }
+                                    }
+                            ?>
                 </div>
 
                 <div class="chat-form">
